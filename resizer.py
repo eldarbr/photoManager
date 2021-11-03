1762170fb9c000bd2dde2bbe914cbb3fdf06ab63
Eldar <35378787+eldarbr@users.noreply.github.com>
2021-11-03 22:42:00 +0300

    Initial - Uploader module

--- resizer.py
+++ resizer.py
@@ -1,15 +1,16 @@
-from PIL import Image, JpegImagePlugin as JIP
+from PIL import Image
 from configparser import ConfigParser
 import argparse
 import os
 
 
-def resize(source_file, max_side_size, quality, target_file):
+def resize(source_file, max_side, max_side_size, quality, target_file):
     """
     Resizes given jpeg according to specified and saves it to target file
 
     :param source_file: input image path
     :param max_side_size: size export settings
+    :param max_side: side of image to be restricted
     :param quality: quality export settings
     :param target_file: output image path
     :return: 0 - no errors; 1 - wrong input image format
@@ -22,21 +23,39 @@ def resize(source_file, max_side_size, quality, target_file):
             return 1
         source_width, source_height = image.width, image.height
         aspect = source_width / source_height
-        if max(source_width, source_height) > max_side_size:
-            if aspect > 1:
-                new_size = (max_side_size, round(max_side_size / aspect))
-            else:
-                new_size = (round(max_side_size * aspect), max_side_size)
 
-            downscaled_image = image.resize(new_size, resample=Image.LANCZOS)
+        if max_side == "w":
+            if source_width > max_side_size:
+                new_size = (max_side_size, round(max_side_size / aspect))
+                downscaled_image = image.resize(new_size, resample=Image.LANCZOS)
+            else:
+                downscaled_image = image
+
+        elif max_side == "h":
+            if source_height > max_side_size:
+                new_size = (round(max_side_size * aspect), max_side_size)
+                downscaled_image = image.resize(new_size, resample=Image.LANCZOS)
+            else:
+                downscaled_image = image
+
+        elif max_side == "m":
+            if max(source_width, source_height) > max_side_size:
+                if aspect > 1:
+                    new_size = (max_side_size, round(max_side_size / aspect))
+                else:
+                    new_size = (round(max_side_size * aspect), max_side_size)
+                downscaled_image = image.resize(new_size, resample=Image.LANCZOS)
+            else:
+                downscaled_image = image
         else:
-            downscaled_image = image
+            raise Exception('Wrong max side property - not in range ["h", "w", "m"]')
+
         if "exif" in image.info:
             exif = image.info['exif']
         else:
             exif = b""
         downscaled_image.save(target_file, 'jpeg', quality=quality, icc_profile=image.info.get('icc_profile'),
-                              progressive=True, subsampling=JIP.get_sampling(image), exif=exif)
+                              progressive=True, subsampling=0, exif=exif)
         return 0
 
 
@@ -49,7 +68,10 @@ def parse_config():
     """
     config = ConfigParser()
     config.read("config.ini")
-    export_presets = [tuple(map(int, x.split(":"))) for x in config["RESIZER"]["newSizes"].replace(" ", "").split(",")]
+    export_presets = []
+    for x in config["RESIZER"]["newSizes"].replace(" ", "").split(","):
+        g = x.split(":")
+        export_presets += [(g[0], int(g[1]), int(g[2]))]
     temp_folder = config["GENERIC"]["temporaryFolder"]
     if not os.path.exists(temp_folder):
         os.mkdir(temp_folder)
@@ -67,9 +89,10 @@ def auto_resize(image_path):
 
     for preset in export_presets:
         file_name = image_path.split("\\")[-1]
-        size, quality = preset
-        resize(image_path, size, quality,
-               target_folder+"\\"+file_name.replace(".", "_"+str(size)+"."))  # insert size suffix to target file name
+        side, size, quality = preset
+        resize(image_path, side, size, quality,
+               target_folder + "\\" + file_name.replace(".", "_" + str(size) + "_" + str(quality) + "."))
+                                            # insert size-quality suffix to target file name
 
 
 def multiple_resize(image_paths_array):
@@ -80,11 +103,11 @@ def multiple_resize(image_paths_array):
     :param image_paths_array: array of input images/folders paths
     """
     for element in image_paths_array:
-        if os.path.isfile(element):                                 # if given path is a file, resize it
+        if os.path.isfile(element):  # if given path is a file, resize it
             auto_resize(element)
         else:
-            paths = [element+"\\"+i for i in os.listdir(element)]
-            multiple_resize(paths)                                  # else resize any file in given folder
+            paths = [element + "\\" + i for i in os.listdir(element)]
+            multiple_resize(paths)  # else resize any file in given folder
 
 
 if __name__ == '__main__':
--- uploader.py
+++ uploader.py
@@ -1,6 +1,7 @@
 import requests
 from configparser import ConfigParser
 import argparse
+import json
 
 
 def parse_config():
@@ -12,10 +13,9 @@ def parse_config():
     """
     config = ConfigParser()
     config.read("config.ini")
-    oauth_token = config["Y.DISK"]["OAuthToken"]
+    oauth_token = config["Y.DISK"]["oauthtoken"]
     main_folder = config["Y.DISK"]["mainFolder"]
-    backup = config.getboolean("Y.DISK", "backup")
-    return oauth_token, main_folder, backup
+    return oauth_token, main_folder
 
 
 def upload(file, token, target_folder):
@@ -25,7 +25,26 @@ def upload(file, token, target_folder):
     invite = requests.get("https://cloud-api.yandex.net/v1/disk/resources/upload",
                           params=[("path", target_folder+filename)],
                           headers={"Authorization": "OAuth "+token})
+    invite_response = json.loads(invite.text)
+
+    uploading = requests.request(invite_response["method"], invite_response["href"], data=open(file, 'rb'),
+                                 headers={"Authorization": "OAuth " + token})
+    if uploading.status_code == 201:
+        return 0
+    else:
+        print("error while uploading")
+        print(uploading.status_code, uploading.reason)
+        print(uploading.text)
+        return 1
+
+
+def auto_upload(file):
+    token, target_folder = parse_config()
+    return upload(file, token, target_folder)
 
 
 if __name__ == '__main__':
-    print("aboba")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image', nargs=1)
+    args = parser.parse_args()
+    auto_upload(args.image[0])
